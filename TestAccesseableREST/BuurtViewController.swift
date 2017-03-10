d4ac2daa5f18059a9a1f8e2d9ddb9112fb3580a0
mobapp06 <mobapp06@dt-c202-m04.local>
2017-03-10 10:06:23 +0100

    cleaned project structure

---
+++ TestAccesseableREST/BuurtViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BuurtViewController.swift
+//  TestAccesseableREST
+//
+//  Created by mobapp06 on 09/03/17.
+//  Copyright © 2017 mobapp06. All rights reserved.
+//
+
+import UIKit
+
+class BuurtViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TestAccesseableREST/InfoViewController.swift
@@ -0,0 +1,36 @@
+//
+//  InfoViewController.swift
+//  TestAccesseableREST
+//
+//  Created by mobapp06 on 09/03/17.
+//  Copyright © 2017 mobapp06. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    @IBOutlet weak var tvInfo: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
