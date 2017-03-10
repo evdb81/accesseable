3de1732aae88ffa947856bd2b5d0b80a17768d3e
mobapp06 <mobapp06@dt-c202-m04.local>
2017-03-10 12:57:37 +0100

    vrijdag 12u50

---
+++ TestAccesseableREST/BuurtCategorieënViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BuurtCategorieënViewController.swift
+//  TestAccesseableREST
+//
+//  Created by mobapp06 on 10/03/17.
+//  Copyright © 2017 mobapp06. All rights reserved.
+//
+
+import UIKit
+
+class BuurtCategoriee_nViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TestAccesseableREST/BuurtViewController.swift
+++ TestAccesseableREST/BuurtViewController.swift
@@ -7,20 +7,56 @@
 //
 
 import UIKit
-
+import MapKit
 
 class BuurtViewController: UIViewController {
-
-
+    
+    
+    @IBOutlet weak var mapview: MKMapView!
+    var locationManager = CLLocationManager.init()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        createRegion()
     }
+    
+    //regio om op te focussen
+    func createRegion()
+    {
+        //begin met middelpunt aan de kust
+        let center = CLLocationCoordinate2DMake(51.2220, 2.9288)
+        //spanweide: bepalen hoeveel inzoomen
+        let span = MKCoordinateSpan.init(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion.init(center: center, span: span)
+        //aanspreken van middelpunt op kaart
+        mapview.region = region
+    }
+    
 
-  
+    //maptypes instellen
+    @IBAction func mapTypeChosen(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0: mapview.mapType = .standard
+        case 1: mapview.mapType = .satellite
+        case 2: mapview.mapType = .hybrid
+        default:
+            break
+        }
+         }
+
     
-    
-    
-    
+    func checkLocationAuthorization()
+    {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse
+        { //positie weergeven op kaart
+            //!!!!! niet vergeten toestemming in info.plist toe te voegen
+            mapview.showsUserLocation = true
+        }else
+        { //toestemming vragen
+            locationManager.requestWhenInUseAuthorization()
+            mapview.showsUserLocation = true
+        }
+        }
+       
+       
 }
