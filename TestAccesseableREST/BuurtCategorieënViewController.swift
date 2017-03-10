0a4e4f9b52c4fc08f550c95a11e38803803b7669
mobapp06 <mobapp06@dt-c202-m04.local>
2017-03-10 13:29:14 +0100

    vrijdag 13u30

--- TestAccesseableREST/BuurtCategorieënViewController.swift
+++ TestAccesseableREST/BuurtCategorieënViewController.swift
@@ -8,28 +8,34 @@
 
 import UIKit
 
-class BuurtCategoriee_nViewController: UIViewController {
-
+class BuurtCategorieenViewController: UIViewController {
+    
+    @IBOutlet weak var swReCa: UISwitch!
+    @IBOutlet weak var swToilet: UISwitch!
+    @IBOutlet weak var swMusea: UISwitch!
+    @IBOutlet weak var swInfopunten: UISwitch!
+    @IBOutlet weak var swHotels: UISwitch!
+    @IBOutlet weak var swParking: UISwitch!
+    @IBOutlet weak var swHellingen: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+
+    @IBAction func zoekenOpCategorie(_ sender: Any) {
+        
+        if {
+        
+        }
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
