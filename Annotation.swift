494b1dbe27adeb22dff760cecc0cd3d136b6258b
mobapp06 <mobapp06@dt-c202-m12.local>
2017-03-14 17:01:15 +0100

    blabla

--- Annotation.swift
+++ Annotation.swift
@@ -14,55 +14,55 @@ public class MyAnnotation:NSObject, MKAnnotation
 {
     public var coordinate: CLLocationCoordinate2D
     public var title: String?
-    public var logo:String = "PlaceIcon"
+    public var pinColor: UIColor
     
     
     init(info:Infokantoor)
     {
         self.coordinate = CLLocationCoordinate2DMake(info.lat, info.lon)
         self.title = info.naam
-        self.logo = Info.png
+        self.pinColor = UIColor.yellow
     }
-    
     
     init(log:Logement)
     {
         self.coordinate = CLLocationCoordinate2DMake(log.lat, log.lon)
         self.title = log.naam
+        self.pinColor = UIColor.green
     }
-    
     
     init(poi:POI)
     {
         self.coordinate = CLLocationCoordinate2DMake(poi.lat, poi.lon)
         self.title = poi.naam
+        self.pinColor = UIColor.red
     }
-    
     
     init(reca:Restaurant)
     {
         self.coordinate = CLLocationCoordinate2DMake(reca.lat, reca.lon)
         self.title = reca.naam
+        self.pinColor = UIColor.blue
     }
- 
     
     init(san:Sanitair)
     {
         self.coordinate = CLLocationCoordinate2DMake(san.lat, san.lon)
         self.title = san.naam
+        self.pinColor = UIColor.gray
     }
-    
     
     init(halte:Tramhalte) {
         self.coordinate = CLLocationCoordinate2DMake(halte.stop_lat, halte.stop_lon)
         self.title = halte.stop_name
+        self.pinColor = UIColor.black
     }
-
     
     init(vpp:VPP)
     {
         self.coordinate = CLLocationCoordinate2DMake(vpp.lat, vpp.lon)
         self.title = "parkeerplaats"
+        self.pinColor = UIColor.cyan
     }
    
     
